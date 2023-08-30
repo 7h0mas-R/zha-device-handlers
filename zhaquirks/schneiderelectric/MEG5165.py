86e4ec9f2facedc4e98230b6682ab5645494fe3b
7h0mas-R <thomas.rohleder@gmx.net>
2023-08-30 22:57:34 +0200

    reverse percentage

--- zhaquirks/schneiderelectric/MEG5165.py
+++ zhaquirks/schneiderelectric/MEG5165.py
@@ -23,6 +23,58 @@ from zhaquirks.const import (
     PROFILE_ID,
 )
 
+ATTR_CURRENT_POSITION_LIFT_PERCENTAGE = 0x0008
+CMD_GO_TO_LIFT_PERCENTAGE = 0x0005
+
+
+class SchneiderWindowCovering(CustomCluster, WindowCovering):
+    """Schneider Electric Window covering cluster."""
+
+    def _update_attribute(self, attrid, value):
+        if attrid == ATTR_CURRENT_POSITION_LIFT_PERCENTAGE:
+            # Invert the percentage value (cf https://github.com/dresden-elektronik/deconz-rest-plugin/issues/3757)
+            value = 100 - value
+        super()._update_attribute(attrid, value)
+
+    async def command(
+        self, command_id, *args, manufacturer=None, expect_reply=True, tsn=None
+    ):
+        """Override default command to invert percent lift value."""
+        if command_id == CMD_GO_TO_LIFT_PERCENTAGE:
+            percent = args[0]
+            # Invert the percentage value
+            percent = 100 - percent
+            v = (percent,)
+            return await super().command(command_id, *v)
+        return await super().command(
+            command_id,
+            *args,
+            manufacturer=manufacturer,
+            expect_reply=expect_reply,
+            tsn=tsn,
+        )
+
+
+class SchneiderElectricCustomCluster(CustomCluster):
+    """Schneider Electric Settings Cluster (0xFF17)."""
+
+    name = "Schneider Electric Settings Cluster."
+    cluster_id = 0xFF17
+
+    attributes = CustomCluster.attributes.copy()
+    attributes.update(
+        {
+            # Alarm settings
+            0x0000: ("led_settings", t.enum8, 0),
+            0x0001: ("button_mode", t.enum8, 3),
+            0x0010: ("unknown1", t.uint8_t, 0),
+            0x0011: ("unknown2", t.uint16_t, 0),
+            0x0020: ("unknown3", t.uint8_t, 1),
+            0x0021: ("unknown4", t.uint16_t, 0),
+            0xFFFD: ("unknown5", t.uint16_t, 1),
+        }
+    )
+
 
 class WiserShutterInsertMEG5165(CustomDevice):
     """1GANG/SHUTTER/1"""
@@ -42,15 +94,15 @@ class WiserShutterInsertMEG5165(CustomDevice):
                 PROFILE_ID: zha.PROFILE_ID,
                 DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
                 INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Groups.cluster_id,
-                    Scenes.cluster_id,
-                    WindowCovering.cluster_id,
-                    Diagnostic.cluster_id,
+                    Basic.cluster_id,  # 0x0000
+                    Identify.cluster_id,  # 0x0003
+                    Groups.cluster_id,  # 0x0004
+                    Scenes.cluster_id,  # 0x0005
+                    WindowCovering.cluster_id,  # 0x0102
+                    Diagnostic.cluster_id,  # 0x0b05
                 ],
                 OUTPUT_CLUSTERS: [
-                    Ota.cluster_id,
+                    Ota.cluster_id,  # 0x0019
                 ],
             },
             # <SimpleDescriptor endpoint=21 profile=260 device_type=260
@@ -61,76 +113,56 @@ class WiserShutterInsertMEG5165(CustomDevice):
                 PROFILE_ID: zha.PROFILE_ID,
                 DEVICE_TYPE: zha.DeviceType.DIMMER_SWITCH,
                 INPUT_CLUSTERS: [
-                    Basic.cluster_id,
-                    Identify.cluster_id,
-                    Diagnostic.cluster_id,
+                    Basic.cluster_id,  # 0x0000
+                    Identify.cluster_id,  # 0x0003
+                    Diagnostic.cluster_id,  # 0x0b05
+                    0xFF17,  # Schneider specific settings
                 ],
                 OUTPUT_CLUSTERS: [
-                    Identify.cluster_id,
-                    Scenes.cluster_id,
-                    OnOff.cluster_id,
-                    LevelControl.cluster_id,
-                    Ota.cluster_id,
-                    WindowCovering.cluster_id,
+                    Identify.cluster_id,  # 0x0003
+                    Scenes.cluster_id,  # 0x0005
+                    OnOff.cluster_id,  # 0x0006
+                    LevelControl.cluster_id,  # 0x0008
+                    Ota.cluster_id,  # 0x0019
+                    WindowCovering.cluster_id,  # 0x0102
                 ],
             },
         },
     }
-    # replacement = {
-    #     SKIP_CONFIGURATION: True,
-    #     ENDPOINTS: {
-    #         1: {
-    #             PROFILE_ID: zha.PROFILE_ID,
-    #             DEVICE_TYPE: zha.DeviceType.SMART_PLUG,
-    #             INPUT_CLUSTERS: [
-    #                 BasicCluster,
-    #                 PowerConfiguration.cluster_id,
-    #                 DeviceTemperature.cluster_id,
-    #                 Groups.cluster_id,
-    #                 Identify.cluster_id,
-    #                 OnOff.cluster_id,
-    #                 Scenes.cluster_id,
-    #                 BinaryOutput.cluster_id,
-    #                 Time.cluster_id,
-    #                 ElectricalMeasurementCluster,
-    #             ],
-    #             OUTPUT_CLUSTERS: [Ota.cluster_id, Time.cluster_id],
-    #         },
-    #         2: {
-    #             PROFILE_ID: zha.PROFILE_ID,
-    #             DEVICE_TYPE: zha.DeviceType.MAIN_POWER_OUTLET,
-    #             INPUT_CLUSTERS: [AnalogInputCluster],
-    #             OUTPUT_CLUSTERS: [AnalogInput.cluster_id, Groups.cluster_id],
-    #         },
-    #         3: {
-    #             PROFILE_ID: zha.PROFILE_ID,
-    #             DEVICE_TYPE: zha.DeviceType.METER_INTERFACE,
-    #             INPUT_CLUSTERS: [AnalogInput.cluster_id],
-    #             OUTPUT_CLUSTERS: [AnalogInput.cluster_id],
-    #         },
-    #         100: {
-    #             PROFILE_ID: zha.PROFILE_ID,
-    #             DEVICE_TYPE: zha.DeviceType.OCCUPANCY_SENSOR,
-    #             INPUT_CLUSTERS: [BinaryInput.cluster_id],
-    #             OUTPUT_CLUSTERS: [BinaryInput.cluster_id, Groups.cluster_id],
-    #         },
-    #     },
-    # }
-
-
-class SchneiderSettingsCluster(CustomCluster):
-    """Schneider Electric Settings Cluster (0xFF17)."""
-
-    name = "Schneider Electric Settings Cluster."
-    cluster_id = 0xFF17
-
-    attributes = {
-        # Alarm settings
-        0x0000: ("led_settings", t.enum8, True),
-        0x0001: ("button_mode", t.enum8, True),
-        0x0010: ("unknown1", t.uint8_t, True),
-        0x0011: ("unknown2", t.uint16_t, True),
-        0x0020: ("unknown3", t.uint8_t, True),
-        0x0021: ("unknown4", t.uint16_t, True),
-        0xFFFD: ("unknown5", t.uint16_t, True),
+    replacement = {
+        ENDPOINTS: {
+            5: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.WINDOW_COVERING_DEVICE,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,  # 0x0000
+                    Identify.cluster_id,  # 0x0003
+                    Groups.cluster_id,  # 0x0004
+                    Scenes.cluster_id,  # 0x0005
+                    SchneiderWindowCovering,  # invert position
+                    Diagnostic.cluster_id,  # 0x0b05
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,  # 0x0019
+                ],
+            },
+            21: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.DIMMER_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,  # 0x0000
+                    Identify.cluster_id,  # 0x0003
+                    Diagnostic.cluster_id,  # 0x0b05
+                    SchneiderElectricCustomCluster,  # Schneider specific settings
+                ],
+                OUTPUT_CLUSTERS: [
+                    Identify.cluster_id,  # 0x0003
+                    Scenes.cluster_id,  # 0x0005
+                    OnOff.cluster_id,  # 0x0006
+                    LevelControl.cluster_id,  # 0x0008
+                    Ota.cluster_id,  # 0x0019
+                    WindowCovering.cluster_id,  # 0x0102
+                ],
+            },
+        },
     }
